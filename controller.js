1041faefd531ac4e274bccb940a680219422241a
rezafaisal98 <rezha.faizal567@gmail.com>
2022-08-29 01:41:24 +0800

    revisi lagi

--- controller.js
+++ controller.js
@@ -46,26 +46,26 @@ const getBook = (res, h) => {
 const addBook = (res, h) => {
    if(res.payload.name == null) return h.response({
       status: "fail",
-      message: "Gagal menambahkan buku, Mohon isi nama buku"
+      message: "Gagal menambahkan buku. Mohon isi nama buku"
    }).code(400)
    const {name, year, author, summary, publisher, pageCount, readPage, reading} = res.payload
    
    const id = nanoid(10)
-   let finished = false
    if(readPage > pageCount) return h.response(replyError("fail", "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount")).code(400)
+   let finished = false
    if(pageCount == readPage) finished = true
    const insertedAt = new Date().toISOString();
    const updatedAt = insertedAt
 
    const newBook = {
-      id, name, year, author, summary, publisher, pageCount, readPage, reading, insertedAt, updatedAt
+      id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
    }
 
    books.push(newBook)
 
    const success = books.filter((book)=> book.id == id).length > 0
    if(success){
-      return h.response(replySuccess("success", {bookId: id})).code(201)
+      return h.response(replySuccess("success", "Buku berhasil ditambahkan", {bookId: id})).code(201)
    }
    return h.response(
       replyError("error", "Buku gagal ditambahkan")
@@ -75,13 +75,13 @@ const addBook = (res, h) => {
 const editBook = (res, h) => {
    if(res.payload.name == null) return h.response({
       status: "fail",
-      message: "Gagal menambahkan buku, Mohon isi nama buku"
+      message: "Gagal memperbarui buku. Mohon isi nama buku"
    }).code(400)
    const id = res.params.id
    const {name, year, author, summary, publisher, pageCount, readPage, reading} = res.payload
 
    let finished = false
-   if(readPage > pageCount) return h.response(replyError("fail", "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount")).code(400)
+   if(readPage > pageCount) return h.response(replyError("fail", "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount")).code(400)
    if(pageCount == readPage) finished = true
    const updatedAt = new Date().toISOString()
    
@@ -104,13 +104,13 @@ const deleteBook = (res, h) => {
    const id = res.params.id
    const index = books.findIndex(item => item.id == id)
    if(books.filter((book)=> book.id == id).length == 0){
-      return h.response(replyError("fail", 'Buku gagal dihapus. Id tidak ditemukan'))
+      return h.response(replyError("fail", "Buku gagal dihapus. Id tidak ditemukan")).code(404)
    }
    if(index >= 0){
       books.splice(index, 1)
-      return h.response(replySuccess("success", 'Buku berhasil dihapus'))
+      return h.response(replySuccess("success", 'Buku berhasil dihapus')).code(200)
    }
-      return h.response(replyError("error", 'Buku gagal dihapus'))
+      return h.response(replyError("error", 'Buku gagal dihapus')).code(500)
 
 }
    
